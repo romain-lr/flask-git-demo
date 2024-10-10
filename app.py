1da3e19419d62c82ab6e15faf7d944578c94370b
romain-lr <romainn.laurie@gmail.com>
2024-10-10 09:02:03 +0200

    Add age calculation feature

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+def calculate_age(dob):
+    today = datetime.today()
+    birth_date = datetime.strptime(dob, "%Y-%m-%d")
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
         dob = request.form['dob']
-        welcome_message = f"Welcome, {name}! Your date of birth is {dob}."
+        age = calculate_age(dob)
+        welcome_message = f"Welcome, {name}! You are {age} years old."
         return render_template('result.html', message=welcome_message)
     return render_template('index.html')
 
